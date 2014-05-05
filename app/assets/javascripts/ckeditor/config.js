0aa892f49c39fe15c08f6a515707b4424bbe44d0
Steven Tseng <booda406@gmail.com>
2014-05-05 23:43:32 +0800

    minimize ckeditor toolbar

---
+++ app/assets/javascripts/ckeditor/config.js
@@ -0,0 +1,16 @@
+CKEDITOR.editorConfig = function( config )
+{
+  config.toolbar = 'Mini';
+  config.uiColor = '#9AB8F3';
+
+  config.toolbar_Mini =
+    [
+        ['Cut','Copy','Paste',], ['Undo','Redo'],
+        { name: 'insert', items : [ 'Image','Table','HorizontalRule','Smiley','SpecialChar','Iframe' ] },
+        '/',
+        { name: 'styles', items : [ 'Styles','Format' ] },
+        { name: 'links', items : [ 'Link','Unlink' ] },
+       	['Bold','Italic','Underline','Strike'], ['NumberedList','BulletedList','-','Outdent','Indent','Blockquote'],
+        { name: 'tools', items : [ 'Maximize' ] }
+    ];
+}
